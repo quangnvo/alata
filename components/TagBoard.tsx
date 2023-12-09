259ebd5c2e452e2e9d2ffc90e270f1170b81bac6
quangnvo <voquang.usth@gmail.com>
2023-12-09 08:26:08 +0800

    add the basic Edit mode and delete button

--- components/TagBoard.tsx
+++ components/TagBoard.tsx
@@ -51,20 +51,22 @@ const TagBoard = () => {
 		'Travel Safety',];
 
 	const [textValue, setTextValue] = useState<string>('');
+	const [editMode, setEditMode] = useState<boolean>(false);
+	const [tags, setTags] = useState<string[]>(initialTags);
 
-	const renderTags = () => {
-		return <div className='flex flex-wrap gap-3'>
-			{initialTags.map((tag) => (
-				<Button variant="outline" onClick={() => handleTagClick(tag)}>
-					{tag}
-				</Button>
-			))}
-		</div>
-	};
+	// const renderTags = () => {
+	// 	return <div className='flex flex-wrap gap-3'>
+	// 		{initialTags.map((tag) => (
+	// 			<Button variant="outline" onClick={() => handleTagClick(tag)}>
+	// 				{tag}
+	// 			</Button>
+	// 		))}
+	// 	</div>
+	// };
+
 
 	const handleTagClick = (tag: string) => {
 		const tags = textValue.split(',').map((t) => t.trim()); // Split existing tags
-
 		if (tags.includes(tag)) {
 			const updatedTags = tags.filter((t) => t !== tag); // Remove the tag
 			const newTextValue = updatedTags.length > 0 ? updatedTags.join(', ') : ''; // Update the text without the removed tag
@@ -89,11 +91,64 @@ const TagBoard = () => {
 		navigator.clipboard.writeText(textValue);
 	};
 
+	const handleEditClick = () => {
+		setEditMode(!editMode);
+	};
+
+	const handleTagDelete = (tagToDelete: string) => {
+		const updatedTags = tags.filter((tag) => tag !== tagToDelete);
+		setTags(updatedTags);
+	};
+
+	const renderTags = () => {
+		return tags.map((tag) => (
+			<div key={tag} className="inline-block">
+				{/* {editMode && (
+					<button
+						className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded-full text-sm ml-1"
+						onClick={() => handleTagDelete(tag)}
+					>
+						X
+					</button>
+				)}
+				<button
+					className={`${textValue.split(',').map((t) => t.trim()).includes(tag) ? 'font-bold bg-black' : ''
+						} bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded inline-block`}
+					onClick={() => handleTagClick(tag)}
+				>
+					{tag}
+				</button> */}
+
+				<button
+					className={`relative ${textValue.split(',').map((t) => t.trim()).includes(tag) ? 'font-bold bg-black' : ''
+						} bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-block`}
+					onClick={() => handleTagClick(tag)}
+				>
+					{tag}
+					{editMode && (
+						<button
+							className="absolute top-0 right-0 bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded-full text-sm"
+							onClick={() => handleTagDelete(tag)}
+						>
+							X
+						</button>
+					)}
+				</button>
+			</div>
+		));
+	};
+
 	return (
 		<div className="grid grid-cols-12 gap-4">
 
-			{/* Tags */}
 			<div className="col-span-9">
+
+				{/* Button Edit */}
+				<Button className='mb-2' onClick={handleEditClick}>
+					{editMode ? 'Done' : 'Edit'}
+				</Button>
+
+				{/* Tags */}
 				{renderTags()}
 			</div>
 
