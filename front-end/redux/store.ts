1b7b5797031a03e94be586b2faf72a2d523f3162
quangnvo <voquang.usth@gmail.com>
2024-02-04 17:33:16 +0100

    remove the counter slice

--- front-end/redux/store.ts
+++ front-end/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './features/counterSlice'
 import tagReducer from './features/tagSlice'
 
 export const store = configureStore({
 	reducer: {
-		counterReducer,
 		tagReducer,
 	},
 })
