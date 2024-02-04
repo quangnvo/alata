7cc8b30583eb86fff283c2e8ff5aef8fb1a50739
quangnvo <voquang.usth@gmail.com>
2024-02-04 11:04:39 +0100

    add the function Add new section

--- front-end/components/TagBoard.tsx
+++ front-end/components/TagBoard.tsx
@@ -1,26 +1,70 @@
+"use client"
+
+import { useState } from 'react';
 import { Button } from "@/components/ui/button"
 import TagSection from './TagSection';
 import TagTextAddedArea from './TagTextAddedArea';
 import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { Input } from "@/components/ui/input"
 import { addSection } from "@/redux/features/tagSlice";
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+	DialogFooter,
+} from "@/components/ui/dialog"
+
 
 const TagBoard = () => {
 	const { tagSection } = useAppSelector((state) => state.tagReducer);
 	const dispatch = useAppDispatch();
 
-	const handleAddSectionClick = () => {
+	const [newSectionName, setNewSectionName] = useState('');
+
+	const handleFormSubmit = () => {
 		const newSection = {
-			sectionName: 'New Section',
-			tags: [],
+			sectionName: newSectionName,
+			tags: [],  // Initialize tags as an empty array
 		};
 		dispatch(addSection(newSection));
+		setNewSectionName('');
 	};
 
 	return (
 		<div>
-			<Button className='mb-5' onClick={handleAddSectionClick}>
-				Add new section
-			</Button>
+			<Dialog>
+				<DialogTrigger asChild className='mb-5'>
+					<Button>
+						Add new section
+					</Button>
+				</DialogTrigger>
+
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>
+							Add new section
+						</DialogTitle>
+					</DialogHeader>
+					<div className="grid gap-4 py-4">
+						<label>
+							<Input
+								type="text"
+								value={newSectionName}
+								onChange={e => setNewSectionName(e.target.value)}
+								placeholder="Section name"
+							/>
+						</label>
+					</div>
+
+					<DialogFooter>
+						<Button variant="outline">Cancel</Button>
+						<Button type="submit" onClick={handleFormSubmit}>Save</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+
 
 			<div className="grid grid-cols-12 gap-4">
 				{tagSection.map((section, index) => (
